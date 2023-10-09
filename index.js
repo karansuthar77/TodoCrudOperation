1b0ae6d7f5dd3cb0eee8fecd5f5a6b2f969ff049
karansuthar <karansuthar@vasyerp.com>
2023-10-09 11:48:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+document.write("koi kare he re");
+console.warn("tha koni padto ke thAni");
+console.error("sapa kalo he thu dopa");
+var n1=34;
+var n2=73;
+console.log(n1+n2);
+function avg(a,b){
+    return (a+b)/2  ;
+}
+c=avg(4,5);
+console.log(c);
+var arr=[4,56,3,5,3,57];
+arr.forEach(function(element){
+    console.log(element);
+})
+let j=0;
+function loop(){do{
+   
+ 
+    console.log(arr[j]);
+    j++;
+
+}while(j<arr.length);
+}
+loop();
---
+++ index2.js
@@ -0,0 +1,1 @@
+// npm host entry point
